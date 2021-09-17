93e1a8c968cfe53712b21144a6fff082d2479af4
xichen1 <xichen.pan@gmail.com>
2021-09-16 20:28:34 -0600

    print more detail

--- echo_server.py
+++ echo_server.py
@@ -2,32 +2,35 @@
 import socket
 import time
 
-#define address & buffer size
-HOST = ""
+# define address & buffer size
+HOST = "localhost"
 PORT = 8001
 BUFFER_SIZE = 1024
 
+
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    
-        #QUESTION 3
+
+        # QUESTION 3
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        
-        #bind socket to address
+
+        # bind socket to address
         s.bind((HOST, PORT))
-        #set to listening mode
+        # set to listening mode
         s.listen(2)
-        
-        #continuously listen for connections
+
+        # continuously listen for connections
         while True:
             conn, addr = s.accept()
             print("Connected by", addr)
-            
-            #recieve data, wait a bit, then send it back
+
+            # recieve data, wait a bit, then send it back
             full_data = conn.recv(BUFFER_SIZE)
+            print(full_data)
             time.sleep(0.5)
             conn.sendall()
             conn.close()
+
 
 if __name__ == "__main__":
     main()
